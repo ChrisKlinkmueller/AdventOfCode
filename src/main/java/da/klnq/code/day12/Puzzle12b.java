6e5de6673a9eaf1aefa31daadcd20d25f067d9aa
Chris Klinkmueller <christopher.klinkmuller@csiro.au>
2020-12-17 10:39:01 +1100

    solution for day 12

---
+++ src/main/java/da/klnq/code/day12/Puzzle12b.java
@@ -0,0 +1,85 @@
+package da.klnq.code.day12;
+
+import da.klnq.code.util.Tuple2;
+
+public final class Puzzle12b extends MoveInstruction {
+
+    public static void main(String[] args) {
+        final Puzzle12b puzzle = new Puzzle12b();
+        puzzle.solve();
+    }
+
+    private Tuple2<Integer, Integer> wayPoint;
+
+    private Puzzle12b() {
+        this.wayPoint = new Tuple2<>(10, -1);
+        this.position = new Tuple2<>(0, 0);
+    } 
+
+    @Override
+    protected void moveNorth(int value) {
+        this.wayPoint = this.wayPoint.modify(lon -> lon, lat -> lat - value);
+    }    
+
+    @Override
+    protected void moveSouth(int value) {
+        this.wayPoint = this.wayPoint.modify(lon -> lon, lat -> lat + value);
+    }
+
+    @Override
+    protected void moveEast(int value) {
+        this.wayPoint = this.wayPoint.modify(lon -> lon + value, lat -> lat);
+    }
+
+    @Override
+    protected void moveWest(int value) {
+        this.wayPoint = this.wayPoint.modify(lon -> lon - value, lat -> lat);
+    }  
+
+    @Override
+    protected void rotateLeft(int value) {
+        if (value == 180) {
+            this.flipDirection();
+        }
+        else if (value == 90) {
+            this.turnLeft();
+        }
+        else if (value == 270) {
+            this.turnRight();
+        }
+    }
+
+    @Override
+    protected void rotateRight(int value) {
+        if (value == 180) {
+            this.flipDirection();
+        }
+        else if (value == 90) {
+            this.turnRight();
+        }
+        else if (value == 270) {
+            this.turnLeft();
+        }
+    }
+
+    private void flipDirection() {
+        this.wayPoint = this.wayPoint.modify(lon -> -lon, lat -> -lat);
+    }
+
+    private void turnLeft() {
+        this.wayPoint = new Tuple2<>(this.wayPoint.getValue2(), -this.wayPoint.getValue1());
+    }
+
+    private void turnRight() {
+        this.wayPoint = new Tuple2<>(-this.wayPoint.getValue2(), this.wayPoint.getValue1());
+    }
+
+    @Override
+    protected void moveForward(int value) {
+        this.position = this.position.modify(
+            lon -> lon + this.wayPoint.getValue1() * value, 
+            lat -> lat + this.wayPoint.getValue2() * value
+        );
+    }
+
+}
