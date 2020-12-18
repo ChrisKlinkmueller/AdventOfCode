5ff40f44f340a1c754b50d6664d367a3e21a17e6
Chris Klinkmueller <christopher.klinkmuller@csiro.au>
2020-12-18 14:02:01 +1100

    Tuple Uitls added

--- src/main/java/da/klnq/code/util/Tuple4.java
+++ src/main/java/da/klnq/code/util/Tuple4.java
@@ -1,14 +1,16 @@
 package da.klnq.code.util;
 
-public class Tuple3<T,U,V> {
+public class Tuple4<T,U,V,W> {
     private final T value1;
     private final U value2;
     private final V value3;
+    private final W value4;
     
-    public Tuple3(T value1, U value2, V value3) {
+    public Tuple4(T value1, U value2, V value3, W value4) {
         this.value1 = value1;
         this.value2 = value2;
         this.value3 = value3;
+        this.value4 = value4;
     }
 
     public T getValue1() {
@@ -21,5 +23,9 @@ public class Tuple3<T,U,V> {
 
     public V getValue3() {
         return this.value3;
+    }
+
+    public W getValue4() {
+        return this.value4;
     }
 }
