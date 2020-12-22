448567209111de0545a0fb6c2e87486d79d24575
Chris Klinkmueller <christopher.klinkmuller@csiro.au>
2020-12-22 22:09:02 +1100

    Solution for day 22 completed

--- src/main/java/da/klnq/advent/Puzzle22.java
+++ src/main/java/da/klnq/advent/Puzzle22.java
@@ -1,60 +1,119 @@
 package da.klnq.advent;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import da.klnq.util.IOUtils;
-import da.klnq.util.Tuple2;
+import da.klnq.util.Tuple3;
 
 public class Puzzle22 {
     private final static String RESOURCE = "/22-task-input.txt";
-    
+
     public static void main(String[] args) {
         final List<String> input = IOUtils.readResource(RESOURCE);
         System.out.println("Solution for part 1: " + solvePart1(input));
-    }    
+        System.out.println("Solution for part 2: " + solvePart2(input));
+    }
 
-    public static long solvePart1(List<String> input) {
-        final Tuple2<Queue<Long>,Queue<Long>> players = parseInput(input);
+    public static int solvePart1(List<String> input) {
+        return solve(input, Puzzle22::playStandard);
+    }
+
+    public static int solvePart2(List<String> input) {
+        return solve(input, Puzzle22::playRecursive);
+    }
+
+    private static int solve(
+        List<String> input,
+        Function<Tuple3<Queue<Integer>, Queue<Integer>, Boolean>, 
+        Tuple3<Queue<Integer>, Queue<Integer>, Boolean>> rules
+    ) {
+        Tuple3<Queue<Integer>, Queue<Integer>, Boolean> players = parseInput(input);
+        players = rules.apply(players);
+        return players.get3() ? computeScore(players.get1()) : computeScore(players.get2());
+    }
+
+    private static Tuple3<Queue<Integer>, Queue<Integer>, Boolean> playStandard(
+        Tuple3<Queue<Integer>, Queue<Integer>, Boolean> players
+    ) {
         while (!players.get1().isEmpty() && !players.get2().isEmpty()) {
-            final long v1 = players.get1().poll();
-            final long v2 = players.get2().poll();
-            if (v1 < v2) {
-                players.get2().add(v2);
-                players.get2().add(v1);
-            }
-            else {
-                players.get1().add(v1);
-                players.get1().add(v2);
-            }
+            final int c1 = players.get1().poll();
+            final int c2 = players.get2().poll();
+            addCards(players, c1, c2, c1 > c2);
         }
-
-        return players.get1().isEmpty()
-            ? computeScore(players.get2())
-            : computeScore(players.get1());
+        return new Tuple3<>(players.get1(), players.get2(), !players.get1().isEmpty());
     }
 
-    private static long computeScore(Queue<Long> queue) {
-        long score = 0;
-        while (!queue.isEmpty()) {
-            score += queue.size() * queue.poll();
+    private static Tuple3<Queue<Integer>, Queue<Integer>, Boolean> playRecursive(
+        Tuple3<Queue<Integer>, Queue<Integer>, Boolean> players
+    ) {
+        final Set<Integer> history1 = new HashSet<>();
+        final Set<Integer> history2 = new HashSet<>();
+
+        while (!players.get1().isEmpty() && !players.get2().isEmpty()) {
+            if (!history1.add(players.get1().hashCode()) || !history2.add(players.get2().hashCode())) {
+                return new Tuple3<>(players.get1(), players.get2(), true);
+            }
+
+            final int c1 = players.get1().poll();
+            final int c2 = players.get2().poll();
+            boolean firstPlayerWon = c1 > c2;
+            if (c1 <= players.get1().size() && c2 <= players.get2().size()) {
+                firstPlayerWon = playRecursive(copy(players, c1, c2)).get3();
+            }
+            addCards(players, c1, c2, firstPlayerWon);
         }
-        return score;
+        return new Tuple3<>(players.get1(), players.get2(), !players.get1().isEmpty());
     }
 
-    private static Tuple2<Queue<Long>,Queue<Long>> parseInput(List<String> input) {
-        final Queue<Long> player1 = new LinkedList<>();
-        final Queue<Long> player2 = new LinkedList<>();
-        Queue<Long> current = player1;
+    private static Tuple3<Queue<Integer>, Queue<Integer>, Boolean> copy(
+        Tuple3<Queue<Integer>, Queue<Integer>, Boolean> original,
+        int c1,
+        int c2
+    ) {
+        return new Tuple3<>(
+            new LinkedList<>(original.get1().stream().limit(c1).collect(Collectors.toList())),
+            new LinkedList<>(original.get2().stream().limit(c2).collect(Collectors.toList())),
+            original.get3()
+        );
+    }
+
+    private static void addCards(Tuple3<Queue<Integer>, Queue<Integer>, Boolean> players, int c1, int c2, boolean firstPlayerWon) {
+        if (firstPlayerWon) {
+            players.get1().add(c1);
+            players.get1().add(c2);
+        }
+        else {
+            players.get2().add(c2);
+            players.get2().add(c1);
+        }
+    }
+
+    private static Tuple3<Queue<Integer>, Queue<Integer>, Boolean> parseInput(List<String> input) {
+        final Queue<Integer> player1 = new LinkedList<>();
+        final Queue<Integer> player2 = new LinkedList<>();
+        Queue<Integer> current = player1;
         for (String line : input) {
             if (line.isBlank()) {
                 current = player2;
             }
             else if (!line.startsWith("Player")) {
-                current.add(Long.parseLong(line));
+                current.add(Integer.parseInt(line));
             }
         }
-        return new Tuple2<>(player1, player2);
+        return new Tuple3<>(player1, player2, null);
+    }
+
+    private static int computeScore(Queue<Integer> queue) {
+        int score = 0;
+        while (!queue.isEmpty()) {
+            score += queue.size() * queue.poll();
+        }
+        return score;
     }
 }
--- src/test/java/da/klnq/code/TestPuzzle22.java
+++ src/test/java/da/klnq/code/TestPuzzle22.java
@@ -9,11 +9,19 @@ import da.klnq.util.IOUtils;
 
 public class TestPuzzle22 {
     private static final String RESOURCE = "/22-test-input.txt";
-    private static final long TEST_RESULT_PART_1 = 306;
+    private static final int TEST_RESULT_PART_1 = 306;
+    private static final int TEST_RESULT_PART_2 = 291;
 
     @Test
-    public void test() {
-        final long result = Puzzle22.solvePart1(IOUtils.readResource(RESOURCE));
+    public void testPart1() {
+        final int result = Puzzle22.solvePart1(IOUtils.readResource(RESOURCE));
         assertEquals(TEST_RESULT_PART_1, result);
     }
+
+    @Test
+    public void testPart2() {
+        final int result = Puzzle22.solvePart2(IOUtils.readResource(RESOURCE));
+        assertEquals(TEST_RESULT_PART_2, result);
+    }
+
 }
