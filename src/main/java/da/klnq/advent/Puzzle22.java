ea33230a1a5cdb4cd686dd81418609cda3addf11
Chris Klinkmueller <christopher.klinkmuller@csiro.au>
2020-12-22 22:23:38 +1100

    Solution for day 22 refactored

--- src/main/java/da/klnq/advent/Puzzle22.java
+++ src/main/java/da/klnq/advent/Puzzle22.java
@@ -5,11 +5,10 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.Set;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import da.klnq.util.IOUtils;
-import da.klnq.util.Tuple3;
+import da.klnq.util.Tuple2;
 
 public class Puzzle22 {
     private final static String RESOURCE = "/22-task-input.txt";
@@ -21,80 +20,20 @@ public class Puzzle22 {
     }
 
     public static int solvePart1(List<String> input) {
-        return solve(input, Puzzle22::playStandard);
+        return solve(input, false);
     }
 
     public static int solvePart2(List<String> input) {
-        return solve(input, Puzzle22::playRecursive);
+        return solve(input, true);
     }
 
-    private static int solve(
-        List<String> input,
-        Function<Tuple3<Queue<Integer>, Queue<Integer>, Boolean>, 
-        Tuple3<Queue<Integer>, Queue<Integer>, Boolean>> rules
-    ) {
-        Tuple3<Queue<Integer>, Queue<Integer>, Boolean> players = parseInput(input);
-        players = rules.apply(players);
-        return players.get3() ? computeScore(players.get1()) : computeScore(players.get2());
+    private static int solve(List<String> input, boolean recursive) {
+        final Tuple2<Queue<Integer>, Queue<Integer>> players = parseInput(input);
+        final Tuple2<Queue<Integer>, Boolean> result = play(players.get1(), players.get2(), recursive);
+        return computeScore(result.get1());
     }
 
-    private static Tuple3<Queue<Integer>, Queue<Integer>, Boolean> playStandard(
-        Tuple3<Queue<Integer>, Queue<Integer>, Boolean> players
-    ) {
-        while (!players.get1().isEmpty() && !players.get2().isEmpty()) {
-            final int c1 = players.get1().poll();
-            final int c2 = players.get2().poll();
-            addCards(players, c1, c2, c1 > c2);
-        }
-        return new Tuple3<>(players.get1(), players.get2(), !players.get1().isEmpty());
-    }
-
-    private static Tuple3<Queue<Integer>, Queue<Integer>, Boolean> playRecursive(
-        Tuple3<Queue<Integer>, Queue<Integer>, Boolean> players
-    ) {
-        final Set<Integer> history1 = new HashSet<>();
-        final Set<Integer> history2 = new HashSet<>();
-
-        while (!players.get1().isEmpty() && !players.get2().isEmpty()) {
-            if (!history1.add(players.get1().hashCode()) || !history2.add(players.get2().hashCode())) {
-                return new Tuple3<>(players.get1(), players.get2(), true);
-            }
-
-            final int c1 = players.get1().poll();
-            final int c2 = players.get2().poll();
-            boolean firstPlayerWon = c1 > c2;
-            if (c1 <= players.get1().size() && c2 <= players.get2().size()) {
-                firstPlayerWon = playRecursive(copy(players, c1, c2)).get3();
-            }
-            addCards(players, c1, c2, firstPlayerWon);
-        }
-        return new Tuple3<>(players.get1(), players.get2(), !players.get1().isEmpty());
-    }
-
-    private static Tuple3<Queue<Integer>, Queue<Integer>, Boolean> copy(
-        Tuple3<Queue<Integer>, Queue<Integer>, Boolean> original,
-        int c1,
-        int c2
-    ) {
-        return new Tuple3<>(
-            new LinkedList<>(original.get1().stream().limit(c1).collect(Collectors.toList())),
-            new LinkedList<>(original.get2().stream().limit(c2).collect(Collectors.toList())),
-            original.get3()
-        );
-    }
-
-    private static void addCards(Tuple3<Queue<Integer>, Queue<Integer>, Boolean> players, int c1, int c2, boolean firstPlayerWon) {
-        if (firstPlayerWon) {
-            players.get1().add(c1);
-            players.get1().add(c2);
-        }
-        else {
-            players.get2().add(c2);
-            players.get2().add(c1);
-        }
-    }
-
-    private static Tuple3<Queue<Integer>, Queue<Integer>, Boolean> parseInput(List<String> input) {
+    private static Tuple2<Queue<Integer>, Queue<Integer>> parseInput(List<String> input) {
         final Queue<Integer> player1 = new LinkedList<>();
         final Queue<Integer> player2 = new LinkedList<>();
         Queue<Integer> current = player1;
@@ -106,7 +45,38 @@ public class Puzzle22 {
                 current.add(Integer.parseInt(line));
             }
         }
-        return new Tuple3<>(player1, player2, null);
+        return new Tuple2<>(player1, player2);
+    }
+
+    private static Tuple2<Queue<Integer>, Boolean> play(Queue<Integer> player1, Queue<Integer> player2, boolean recursive) {
+        final Set<Integer> history1 = recursive ? new HashSet<>() : null;
+        final Set<Integer> history2 = recursive ? new HashSet<>() : null;
+
+        while (!player1.isEmpty() && !player2.isEmpty()) {
+            if (recursive && (!history1.add(player1.hashCode()) || !history2.add(player2.hashCode()))) {
+                return new Tuple2<>(player1, true);
+            }
+
+            final int c1 = player1.poll();
+            final int c2 = player2.poll();
+            final boolean firstPlayerWon = recursive && c1 <= player1.size() && c2 <= player2.size()
+                ? play(copy(player1, c1), copy(player2, c2), recursive).get2()
+                : c1 > c2;
+
+            if (firstPlayerWon) {
+                player1.add(c1);
+                player1.add(c2);
+            }
+            else {
+                player2.add(c2);
+                player2.add(c1);
+            }
+        }
+        return new Tuple2<>(!player1.isEmpty() ? player1 : player2, !player1.isEmpty());
+    }
+
+    private static Queue<Integer> copy(Queue<Integer> original, int count ) {
+        return new LinkedList<>(original.stream().limit(count).collect(Collectors.toList()));
     }
 
     private static int computeScore(Queue<Integer> queue) {
