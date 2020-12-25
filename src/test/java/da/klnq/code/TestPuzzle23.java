03ed8e061cbad136b9d39c8b8fa5cba998667414
Chris Klinkmueller <christopher.klinkmuller@csiro.au>
2020-12-25 17:01:23 +1100

    test input for day 23 moved to separate file

--- src/test/java/da/klnq/code/TestPuzzle23.java
+++ src/test/java/da/klnq/code/TestPuzzle23.java
@@ -7,21 +7,22 @@ import java.math.BigInteger;
 import org.junit.jupiter.api.Test;
 
 import da.klnq.advent.Puzzle23;
+import da.klnq.util.IOUtils;
 
 public class TestPuzzle23 {
-    private static final String INPUT = "389125467";
+    private static final String RESOURCE = "/23-test-input.txt";
     private static final String TEST_RESULT_PART_1 = "67384529";
     private static final BigInteger TEST_RESULT_PART_2 = BigInteger.valueOf(149245887792L);
 
     @Test
     public void testPart1() {
-        final String result = Puzzle23.solvePart1(INPUT);
+        final String result = Puzzle23.solvePart1(IOUtils.readResource(RESOURCE).get(0));
         assertEquals(TEST_RESULT_PART_1, result);
     }
 
     @Test
     public void testPart2() {
-        final BigInteger result = Puzzle23.solvePart2(INPUT);
+        final BigInteger result = Puzzle23.solvePart2(IOUtils.readResource(RESOURCE).get(0));
         assertEquals(TEST_RESULT_PART_2, result);
     }
     
